d431f92111a5fe808f902c35ed8cfb0a7d81e736
nostis <norbert_2000@o2.pl>
2019-06-17 19:52:53 +0200

    Prevent from closing app after starting

--- src/main/java/com/nostis/Runnable.java
+++ src/main/java/com/nostis/Runnable.java
@@ -1,11 +1,17 @@
 package com.nostis;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Runnable {
+public class Runnable implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Runnable.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Thread.currentThread().join();
     }
 }
