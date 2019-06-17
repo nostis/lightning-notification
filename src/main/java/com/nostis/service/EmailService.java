f5ba35f47e1983d55fa1b4591f47295b4dd494db
nostis <norbert_2000@o2.pl>
2019-06-17 22:06:47 +0200

    Add e-mail sending functionality

---
+++ src/main/java/com/nostis/service/EmailService.java
@@ -0,0 +1,26 @@
+package com.nostis.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendMail(String to, String subject, String text) throws MessagingException {
+        MimeMessage mail = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text);
+
+        emailSender.send(mail);
+    }
+
+}
