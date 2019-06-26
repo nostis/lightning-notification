5a1f0b9863a6e8684991c1d2f8661e1c250cec42
nostis <norbert_2000@o2.pl>
2019-06-26 14:59:13 +0200

    Fix syntax error when initializing h2 db

--- src/main/java/com/nostis/model/Lightning.java
+++ src/main/java/com/nostis/model/Lightning.java
@@ -1,14 +1,10 @@
 package com.nostis.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-
 import javax.persistence.*;
 import java.util.Calendar;
 import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 @Entity
 @Data
 public class Lightning {
@@ -22,19 +18,19 @@ public class Lightning {
 
     private String direction;
 
-    private int interval;
+    private int inter;
 
     private Calendar created;
 
     @ElementCollection
     private List<Float> area;
 
-    public Lightning(Long id, int count, float distance, String direction, int interval, Calendar created, List<Float> area) {
+    public Lightning(Long id, int count, float distance, String direction, int inter, Calendar created, List<Float> area) {
         this.id = id;
         this.count = count;
         this.distance = distance;
         this.direction = direction;
-        this.interval = interval;
+        this.inter = inter;
         this.created = created;
         this.area = area;
     }
