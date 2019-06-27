e5fc157e4008a1dd3a90ca233eb8e30fe3991742
nostis <norbert_2000@o2.pl>
2019-06-27 18:13:01 +0200

    Change name of properties to be more clear and add test for client

---
+++ src/test/java/com/nostis/ClientCrudIT.java
@@ -0,0 +1,33 @@
+package com.nostis;
+
+import com.nostis.dao.ClientCrud;
+import com.nostis.model.Client;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@ActiveProfiles("h2db")
+@RunWith(SpringRunner.class)
+public class ClientCrudIT {
+    @Autowired
+    private ClientCrud clientCrud;
+
+    @Before
+    public void setUp() {
+        clientCrud.save(new Client(1L, "name", "token"));
+        clientCrud.save(new Client(2L, "name2", "token2"));
+    }
+
+    @Test
+    public void whenFindByName_thenReturnAppropriateClient() {
+        Assert.assertEquals(Long.valueOf(1L), clientCrud.findByName("name").get().getId());
+        Assert.assertEquals("name", clientCrud.findByName("name").get().getName());
+        Assert.assertEquals("token", clientCrud.findByName("name").get().getToken());
+    }
+}
